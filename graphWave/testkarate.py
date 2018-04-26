2080cd9bb85b7a992ad9c608d7051f0a2fafc6d9
CEfanmin <fanminUESTC2015@163.com>
2018-04-26 14:39:21 +0800

    test graphWave

---
+++ graphWave/testkarate.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import networkx as nx 
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+from GraphWave.graphwave import graphwave
+
+def read_graph():
+    '''read graph'''
+    G = nx.read_edgelist('./karate-mirrored.edgelist',nodetype=int, create_using=nx.DiGraph())
+    for edge in G.edges():
+        G[edge[0]][edge[1]]['weight'] = 1
+
+    G = G.to_undirected()
+    return G
+
+def load_data():
+    '''load data'''
+    raw_data = pd.read_csv('./karate-emb.csv')
+    feture_data = raw_data.iloc[:,0:2]
+    id_data = raw_data['id'].tolist()
+    return feture_data, id_data
+
+def visulization(feture_data, id_data):
+    '''Visualize'''
+    fig, ax = plt.subplots()
+    plt.scatter(feture_data['x1'], feture_data['x2'],marker='o',s=150, c=id_data)
+    plt.title('graph wave embedding features')
+    plt.xlabel('1st feature')
+    plt.ylabel('2nd feature')
+
+    for i,txt in enumerate(np.arange(1,69)):
+        ax.annotate(txt,(feture_data['x1'][i],feture_data['x2'][i]),ha='left')
+    ax.grid(True)
+    plt.show()
+
+if __name__=="__main__":
+    '''pipeline'''
+    nx_G = read_graph()
+    nx.draw(nx_G, pos=nx.spring_layout(nx_G))
+    plt.show()
+    chi, heat_print, taus=graphwave(nx_G, 'automatic', verbose=False)
+    print(np.array(chi).shape)
+    pca=PCA(n_components=2)
+    trans_data=pca.fit_transform(StandardScaler().fit_transform(chi))
+    print(trans_data.shape)
+    pd.DataFrame(trans_data).to_csv('./karate-emb.csv')
+
+    feture_data, id_data = load_data()
+    visulization(feture_data, id_data)
+    
--- struc2vec/src/tsneplot.py
+++ struc2vec/src/tsneplot.py
@@ -6,22 +6,27 @@ import seaborn as sns
 
 def load_data():
     '''load data'''
-    raw_data = pd.read_csv('./emb/test.csv')
-    feture_data = raw_data.iloc[:,0:128]
+    raw_data = pd.read_csv('../emb/test3-negative_sampling.csv')
+    feture_data = raw_data.iloc[:,0:2]
     id_data = raw_data['id'].tolist()
     return feture_data, id_data
 
 def visulization(feture_data, id_data):
     '''Visualize'''
-    map_tsne = TSNE(n_components=2, random_state=14)
-    visualized_features = pd.DataFrame(map_tsne.fit_transform(feture_data),columns=['x1','x2'])
-    print(visualized_features)
+    # map_tsne = TSNE(n_components=2, random_state=14)
+    # visualized_features = pd.DataFrame(map_tsne.fit_transform(feture_data),columns=['x1','x2'])
+    # print(visualized_features)
 
     fig, ax = plt.subplots()
-    plt.scatter(visualized_features['x1'], visualized_features['x2'],marker='o',c=id_data, label=id_data)
-    plt.title('the features')
+    plt.scatter(feture_data['x1'], feture_data['x2'],marker='o',s=150, c=id_data)
+    plt.title('graph wave embedding features')
     plt.xlabel('1st feature')
     plt.ylabel('2nd feature')
+
+    for i,txt in enumerate(np.arange(1,69)):
+        ax.annotate(txt,(feture_data['x1'][i],feture_data['x2'][i]),ha='right')
+    
+    # fig.savefig('../karate.png')
     ax.grid(True)
     plt.show()
 
